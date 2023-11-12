d2d6e556b1a7967591fb0b06031e0124d4808e91
zak-dev-fullstack <118931675+zakdevfreelance@users.noreply.github.com>
2023-11-12 17:15:01 -0600

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import './App.css'
 
 function App() {
-
-
   return (
     <>
-   <h1>Portafolio</h1>
-   <h2>Deploy</h2>
+<h1>Portafolio</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error/error-page";
+const router = createBrowserRouter([
+  {
+    path: "/portafolio/",
+    element: <Root />,    
+    errorElement: <ErrorPage />,
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
