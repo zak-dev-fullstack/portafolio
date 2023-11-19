26b0b8ea21d9a296dc44bc1fceb60b60404da986
zak-dev-fullstack <118931675+zakdevfreelance@users.noreply.github.com>
2023-11-19 11:43:08 -0600

    actualizacion-portafolio

--- src/components/subcomponents/portafolio/PortafolioZ.jsx
+++ src/components/subcomponents/portafolio/PortafolioZ.jsx
@@ -1,58 +1,58 @@
-/* eslint-disable no-unused-vars */
-
-import { useState } from "react";
 import "./p.css";
 import imgl from "../../../assets/gifz.gif";
-
+import { Link } from "react-router-dom";
 
 // Datos del portafolio
 const portafolioData = [
-  { title: "Inteligencia Artificial", code: "GitHub" },
-  { title: "Blockchain", code: "GitHub" },
-  { title: "E-commerce", code: "GitHub" },
-  { title: "Admin Place", code: "GitHub" },
-  { title: "Mobile Apps", code: "GitHub" },
-  { title: "Landing Web", code: "GitHub" },
+  { title: "Inteligencia Artificial", code: "https://github.com/zak-dev-fullstack", link: "https://zak-dev-fullstack.github.io/inteligencia-artificial/" },
+  { title: "Blockchain", code: "https://github.com/zak-dev-fullstack", link: "https://zak-dev-fullstack.github.io/blockchain/" },
+  { title: "E-commerce", code: "https://github.com/zak-dev-fullstack", link: "https://zak-dev-fullstack.github.io/e-commerce/" },
+  { title: "Admin Place", code: "https://github.com/zak-dev-fullstack", link: "https://zak-dev-fullstack.github.io/admin-place/" },
+  { title: "Mobile Apps", code: "https://github.com/zak-dev-fullstack", link: "https://zak-dev-fullstack.github.io/mobile-app/" },
+  { title: "Landing Web", code: "https://github.com/zak-dev-fullstack", link: "https://zak-dev-fullstack.github.io/landing-web/" },
 ];
 
 // Componente PortafolioZ
 function PortafolioZ() {
   // Estado para controlar el enlace
-  const [githubLink, setGithubLink] = useState(
-    "https://github.com/zak-dev-fullstack"
-  );
-  const [flippedCard, setFlippedCard] = useState(null);
-
-  const handleCardClick = (index) => {
-    setFlippedCard(index);
-  };
-
-  const handleCardMouseLeave = () => {
-    setFlippedCard(null);
-  };
 
   return (
-<>
-<section className="section-portafolio">
-      {portafolioData.map((project, index) => (
-        <div key={index} className="card-proyecto ">
-          <div className="img-container">
-            <img src={imgl} loading="lazy"  alt={project.title} className="img-proyecto" draggable="false" width="100" height="100"/>
+    <>
+      <section className="section-portafolio">
+        {portafolioData.map((project, index) => (
+          <div key={index} className="card-proyecto">
+            <div className="img-container">
+              <Link to={project.link}>
+                <img
+                  src={imgl}
+                  loading="lazy"
+                  alt={project.title}
+                  className="img-proyecto"
+                  draggable="false"
+                  width="100"
+                  height="100"
+                />
+              </Link>
+            </div>
+            <div className="button-container">
+              <button
+                className="boton-titulo"
+                onClick={() => window.location.href = project.link}
+              >
+                {project.title}
+              </button>
+              <a
+                href={project.code}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="boton-codigo">GitHub</button>
+              </a>
+            </div>
           </div>
-          <div className="button-container">
-            <button
-              className="boton-titulo"
-              onClick={() => (window.location.href = githubLink)}
-            >
-              {project.title}
-            </button>
-            <a  href={githubLink} target="_blank" rel="noopener noreferrer">
-              <button className="boton-codigo">GitHub</button>
-            </a>
-          </div>
-        </div>
-      ))}
-    </section></>
+        ))}
+      </section>
+    </>
   );
 }
 
